6774a2a0d1c6a4dd56cf2eef6d02d461ca639198
Surbhi Bajaj <sb8651@srmist.edu.in>
2026-03-12 00:44:13 +0530

    Refactor banner output to use String array
    Refactored banner printing to use a String array and for-each loop.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,13 +2,21 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", " *** ", " *** ", " ***** ", " ***** "));
-        System.out.println(String.join(" ", "*   *", "*   *", "*    *", "*     "));
-        System.out.println(String.join(" ", "*   *", "*   *", "*    *", "*     "));
-        System.out.println(String.join(" ", "*   *", "*   *", "***** ", " *****"));
-        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "     *"));
-        System.out.println(String.join(" ", "*   *", "*   *", "*     ", "*    *"));
-        System.out.println(String.join(" ", " *** ", " *** ", "*     ", " **** "));
+        // Create String array for banner lines
+        String[] lines = new String[7];
 
+        // Populate array using String.join()
+        lines[0] = String.join(" ", " *** ", " *** ", " ***** ", " ***** ");
+        lines[1] = String.join(" ", "*   *", "*   *", "*    *", "*     ");
+        lines[2] = String.join(" ", "*   *", "*   *", "*    *", "*     ");
+        lines[3] = String.join(" ", "*   *", "*   *", "***** ", " *****");
+        lines[4] = String.join(" ", "*   *", "*   *", "*     ", "     *");
+        lines[5] = String.join(" ", "*   *", "*   *", "*     ", "*    *");
+        lines[6] = String.join(" ", " *** ", " *** ", "*     ", " **** ");
+
+        // Print using for-each loop
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
